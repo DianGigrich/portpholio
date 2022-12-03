80da7db374ec67862ec2a5070293c570c45af1c4
Dian Sherman <slayer_barrett_@hotmail.com>
2022-12-03 12:11:21 -0800

    nav and page router stil nonfunctional

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Header() {
     
     return (
-        <div>
-            <h1>Dian</h1>
+        <div className="container">
+            <h1 className="header">Dian</h1>
         </div>
     )
 }
