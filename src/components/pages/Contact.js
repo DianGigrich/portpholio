215988ce38aaac6f94d86d67279c12d6b56a5a66
Dian Sherman <slayer_barrett_@hotmail.com>
2022-12-03 19:11:06 -0800

    added contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,12 +1,85 @@
 import React from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function Contact() {
-    
-    return (
-        <section class="card">
+ 
+  const [email, setEmail] = useState('');
+  const [userName, setUserName] = useState('');
+  const [message, setMessage] = useState('');
 
-        </section>
-    )
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === 'email') {
+      setEmail(inputValue);
+    } else if (inputType === 'userName') {
+      setUserName(inputValue);
+    } else if (inputType === 'message') {
+        setMessage(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+
+    e.preventDefault();
+
+    if (!validateEmail(email) || !userName) {
+      setErrorMessage('Email is invalid');
+  
+      return;
+   
+    }
+    
+    setUserName('');
+  
+    setEmail('');
+
+    setMessage('');
+    alert(`Thank you for your messsage ${userName}`);
+  };
+
+  return (
+    <div>
+        <form className="form">
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          type="email"
+          placeholder="email"
+        />
+        <input
+          value={userName}
+          name="userName"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Consonant Vowelston"
+        />
+<input
+          value={message}
+          name="message"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="..."
+        />
+        <button type="button" onClick={handleFormSubmit}>
+          Submit
+        </button>
+      </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default Contact;
+
+
