32ebc038f91f79cada3c40111dc87643dd721f57
Dian Sherman <slayer_barrett_@hotmail.com>
2022-12-03 17:05:18 -0800

    moved contact info to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,12 @@ function Footer() {
     
     return (
         <div className="footer fixed-bottom">
-            <p>Created by Dian Gigrich</p>
+                        <h2 id="Contact">Contact Me</h2>
+                <div class="contacts">
+                    <a href="mailto:slayer_barrett_@hotamil.com?subject=I want to hire you.">slayer_barrett_@hotmail.com</a>
+                    <a href="https://github.com/DianGigrich">github</a>
+                    <a href="https://www.linkedin.com/in/dian-sherman/">LinkedIn</a>
+                </div>
         </div>
     )
 }
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,13 +4,7 @@ function Contact() {
     
     return (
         <section class="card">
-            <h2 id="Contact">Contact Me</h2>
-                <div class="contacts">
-                    <a href="tel:253-241-1773">253-241-1773</a>
-                    <a href="mailto:slayer_barrett_@hotamil.com?subject=I want to hire you.">slayer_barrett_@hotmail.com</a>
-                    <a href="https://github.com/DianGigrich">github</a>
-                    <a href="https://www.linkedin.com/in/dian-sherman/">LinkedIn</a>
-                </div>
+
         </section>
     )
 }
