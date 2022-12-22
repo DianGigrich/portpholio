f1009feac7a6dda0bff3423f0916817148d0606f
Dian Sherman <slayer_barrett_@hotmail.com>
2022-12-21 18:14:09 -0800

    added resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -4,11 +4,8 @@ function Resume() {
     return (
         <div className="container mt-3">
             <div class="border rounded p-5 bg-white bg-opacity-50 shadow">
-                <p>I have a bachelor's degree in Interdisciplinary Arts and Sciences from the University of Washington, a certificate from UW as well for Full Stack Web Development.  I have ten years of experience with customer service, working with teams, and learning and teaching technologies in various settings.</p>
-                <p>Full Stack includes: REACT, MERN, Javascript, APIs, MySQL, Sequelize, HTML, CSS, and more</p>
-                <p>Please check my LinkedIn for my qualifications, employment history, and endoresments.</p>
-            <a className="btn border" href="https://www.linkedin.com/in/dian-sherman/" target="_blank">LinkedIn
-            </a>
+            <iframe src="https://resume.creddle.io/embed/8bvaegtjq5c"
+  width="850" height="1100" seamless></iframe>
             </div>
         </div>
     )
