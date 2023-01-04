8cd22ea5e1c6fd7c888e3fe181274ac5211f6538
Dian Sherman <slayer_barrett_@hotmail.com>
2023-01-04 12:52:53 -0800

    editted

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -15,16 +15,16 @@ function AboutMe() {
       </article>
 
       <article className="m-3 row align-items-center">
-        <div className="m-3 p-5 card-body bg-white bg-opacity-75 shadow border border-secondary">
+        <div className="m-3 p-5 card-body bg-white shadow rounded border border-secondary">
           <h3 className="card-title">About Me!</h3>
           <p className="card-text">
-            I am a web developer. I have 7 chickens, 3 cats, 1 dog, and 1 child.  They are all ridiculous in their own special ways.
+            I am a web developer. I have 7 chickens, 3 cats, 1 dog, and 1 medium-sized child.  They are all ridiculous in their own special ways. I am a DIYer and a project seamstress.  I have renovated a shed for chickens, a bathroom, done some questionable electical work, but I find that sewing is the most like coding- it's meticulous and sometimes it doesn't turn out right so you have to rip out the stitches and redo it a few times until it works as intended.
           </p>
           <p className="card-text">
-            I enjoy movie, computer and board games.  I play the piano and teach my daughter, who is mostly interested in learning Christmas music throughout the year. I am a cold body, so I always have on a sweater under my sweatshirt! You would think kitties would keep me warm, but alas, they are more into trouble than snuggles.
+            I play the piano and teach my daughter, who is mostly interested in learning Christmas music throughout the year. I am a cold body, so I always have on a sweater under my sweatshirt! You would think kitties would keep me warm, but alas, they are more into trouble than snuggles. I enjoy movie, computer and board games, and reading. Have you read Brandon Sanderson? He's really quite good.
           </p>
           <p className="card-text">
-            I have been alive for 36 years.  It's strange to say that I am just now starting a career, but plenty of people do that after their kids are in school full time or they decide they don't enjoy their current career.  So lets go off on a big adventure!
+            I have been alive for 36 years, and am just now utilizing my potential... so lets go off on a big adventure!
           </p>
 
         </div>
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -10,59 +10,61 @@ function Portfolio() {
     return (
         <section className="container mt-3">
             <div className="row">
-            <section class="col p-3 m-2 bg-white bg-opacity-50 shadow rounded border border-secondary" id="codescape">
-                    <a href="https://codescape.netlify.com/">
-                        <img className="card-img-top" src={code} alt="Codescape Screenshot" />
+                <section className="col p-3 m-2 bg-white bg-opacity-75 shadow rounded border border-secondary" id="codescape">
+                    <a href="https://codescape.netlify.app/" target="_blank" rel="noopener noreferrer">
+                        <img className="card-img-top border border-secondary rounded" src={code} alt="Codescape Screenshot" />
                     </a>
                     <h3 className="card-title">Codescape</h3>
                     <p>Play escape room mini-games to practice javascript syntax!</p>
                     <a href="https://github.com/DianGigrich/Codescape">
                         Github Repository Front-End
                     </a>
-                    <br/>
+                    <br />
                     <a href="https://github.com/DianGigrich/Codescape-backend">
                         Github Repository Back-End
                     </a>
                     <p>REACT team project. Sequelize, SQL database for backend.</p>
-                    
+
                 </section>
-                <section class="col p-3 m-2 bg-white bg-opacity-50 shadow rounded border border-secondary" id="gloomhaven">
-                    <a href="https://gloomhaven-helper-deluxe.herokuapp.com/">
-                        <img className="card-img-top" src={gloom} alt="Gloomhaven Encounter Screenshot" />
+                <section className="col p-3 m-2 bg-white bg-opacity-75 shadow rounded border border-secondary" id="gloomhaven">
+                    <a href="https://gloomhaven-helper-deluxe.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+                        <img className="card-img-top border border-secondary rounded" src={gloom} alt="Gloomhaven Encounter Screenshot" />
                     </a>
                     <h3 className="card-title">Gloomhaven Helper</h3>
                     <p>Board game assistant to reduce clutter and minutia</p>
                     <a href="https://github.com/Mattdack/gloomhaven-helper">
                         Github Repository
                     </a>
-                    <p>Handlebars, MVC, Sequelize, SQL</p>
+                    <p>Team project, Handlebars, MVC, Sequelize, SQL</p>
                 </section>
 
-                <section className="col p-3 m-2 bg-white bg-opacity-50 shadow rounded border border-secondary" id="pineapple">
-                    <a href="https://diangigrich.github.io/PineappleMotivator/">
-                        <img class="card-img-top" src={hobby} alt="Motivator main page" />
+                <section className="col p-3 m-2 bg-white bg-opacity-75 shadow rounded border border-secondary" id="pineapple">
+                    <a href="https://diangigrich.github.io/PineappleMotivator/" target="_blank" rel="noopener noreferrer">
+                        <img class="card-img-top border border-secondary rounded" src={hobby} alt="Motivator main page" />
                     </a>
                     <h3 className="card-title">Pineapple Motivator</h3>
                     <p>Motivational task manager</p>
                     <a href="https://github.com/DianGigrich/PineappleMotivator">
                         Github Repository
                     </a>
+                    <p>team project, Javascript, Materialize</p>
                 </section>
 
-                <section className="col p-3 m-2 bg-white bg-opacity-50 shadow rounded border border-secondary" id="Catbelly">
-                    <a href="https://DianGigrich.github.io/TriviaQuiz/">
-                        <img className="card-img-top" src={trivia} alt="Spotted Cat Belly" />
+                <section className="col p-3 m-2 bg-white bg-opacity-75 shadow rounded border border-secondary" id="Catbelly">
+                    <a href="https://DianGigrich.github.io/TriviaQuiz/" target="_blank" rel="noopener noreferrer">
+                        <img className="card-img-top border border-secondary rounded" src={trivia} alt="Spotted Cat Belly" />
                     </a>
                     <h3 className="card-title">Genshin Impact Trivia Quiz</h3>
                     <p>Quiz we did as classwork. It's fun.</p>
                     <a href="https://github.com/DianGigrich/TriviaQuiz">
                         Github Repository
                     </a>
+                    <p>Javascript</p>
                 </section>
 
-                <section className="col p-3 m-2 bg-white bg-opacity-50 shadow rounded border border-secondary" id="Catbelly">
-                    <a href="https://diansmvc-tech-blog.herokuapp.com/">
-                        <img className="card-img-top" src={tech} alt="Spotted Cat Belly" />
+                <section className="col p-3 m-2 bg-white bg-opacity-75 shadow rounded border border-secondary" id="Catbelly">
+                    <a href="https://diansmvc-tech-blog.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+                        <img className="card-img-top border border-secondary rounded" src={tech} alt="Spotted Cat Belly" />
                     </a>
                     <h3 className="card-title">Tech Blog</h3>
                     <p>Post and Comment on blogs.</p>
