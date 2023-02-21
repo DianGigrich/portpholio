202a882af87c702bd41982d3faa974a83731b60c
Dian Sherman <slayer_barrett_@hotmail.com>
2023-02-21 11:32:43 -0800

    added color and a tag

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -8,14 +8,14 @@ function Resume() {
     return (
         <>
             <section className="card-body cardborder">
-                <h2>Hello</h2>
+                <h2 className="text-secondary">Hello</h2>
                 <p>DianESherman@gmail.com</p>
-                <h2>Skills</h2>
+                <h2 className="text-primary">Skills</h2>
                 <ul><li>FRONT-END: React, Javascript, HTML5, jQuery, Material-UI, Bootstrap, CSS</li>
                     <li>BACK-END: MySQL, Node.js, MongoDB, Mongoose ODM, Express.js</li>
                     <li>CONCEPTS: APIs, REST, JSON, SSL certificates</li>
                 </ul>
-                <h2>Projects</h2>
+                <h2 className="text-info">Projects</h2>
 
                 <h5><strong>Codescape</strong> - <i>Git Admin</i></h5>
                 <ul>
@@ -39,10 +39,10 @@ function Resume() {
                     <li>A hobby tracker that provides motivation. Get rewarded with a YouTube video related to your motivator.  It can switch to Pirate mode if you're feeling weird.</li>
                     <li>Javascript, HTML, CSS, Materialize</li>
                 </ul>
-                <h2>Education</h2>
+                <h2 className="text-success">Education</h2>
                 <h5><strong>University of Washington, Seattle</strong> - <i>Full Stack Web Developer Certificate</i> - SEP 2022-DEC 2022 </h5>
                 <h5><strong>University of Washington, Tacoma</strong> - <i>Bachelor's in Interdisciplinary A and S</i> - SEP 2006-DEC 2008 </h5>
-                <h2>Employment</h2>
+                <h2 className="text-warning">Employment</h2>
                 <h5><strong>Jyve</strong> - <i>Merchandiser and Trainer</i></h5>
                 <ul>
                     <li>stocked groceries  at two of the most profitable and highly rated Safeway/Albertsons  in the state. Was offered a management position by both store managers.</li>
@@ -57,10 +57,10 @@ function Resume() {
                     <li>reduced building energy consumption by 10%</li>
                     <li>set-up building document storage organization</li>
                 </ul>
-                <h2>Volunteering</h2>
+                <h2 className="text-danger">Volunteering</h2>
                 <h5><strong>Elk Plain School of Choice</strong> - <i>weekly PTA popcorn volunteer</i> - current</h5>
                 <h5><strong>Bethel Recreation</strong> - <i>Assistant Soccer Coach</i> - current</h5>
-                <p>https://elkplain-soccer.netlify.app</p>
+                <p><a href="https://elkplain-soccer.netlify.app">Our Team Soccer Site</a></p>
 
 
 
